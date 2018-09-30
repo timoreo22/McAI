e0ad327a6a566f57a3a0fc1a344dfb09d6ee229c
timoreo22 <timo.oreo34@gmail.com>
2018-09-30 13:45:33 +0200

    Load system!

--- src/test/java/fr/timoreo/mcia/test/IATest.java
+++ src/test/java/fr/timoreo/mcia/test/IATest.java
@@ -2,16 +2,20 @@ package fr.timoreo.mcia.test;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class IATest {
+    public static int[] labels;
+    public static List<int[][]> images;
+
     @Test
     public static void test() {
-
+        load();
     }
 
-    public int[] labels;
-    public int[][] images;
-
-    public static void load(){
-
+    public static void load() {
+        ClassLoader classLoader = IATest.class.getClassLoader();
+        labels = MnistReader.getLabels(classLoader.getResource("train/label/train-labels-idx1-ubyte").getFile());
+        images = MnistReader.getImages(classLoader.getResource("train/images/train-images-idx3-ubyte").getFile());
     }
 }
